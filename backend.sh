a1ff7801fa826a0d87688b22c93a17a2fda96727
Asmember <76887950+Asmember@users.noreply.github.com>
2024-01-11 20:41:49 +0100

    making the grid

--- backend.sh
+++ backend.sh
@@ -1,15 +1,40 @@
-grid = "<table class="game">"
+#!/bin/bash
 
-for i in $(seq 0 2);
+while IFS= read -r line
 do
-    grid += "<tr>"
-    for j in $(seq 0 2);
-    do
-        grid += "<th class=\"borderbottom"
-        if [j = 2] then
-            grid += " borderright"
-        fi
-        grid += "\">"
-    done
-    grid += "</tr>"
-done
+    if [[ "$line" == *"%GameField%"* ]]; then
+        echo "<table class="game">"
+        id=0
+        bigField=0
+
+        for i in $(seq 1 3);
+        do
+            echo "<tr>"
+            for ii in $(seq 1 3);
+            do
+                echo "<th><table class=\".smallField\">"
+                smallField=0
+                for iii in $(seq 1 3);
+                do
+                    echo "<tr>"
+                    for iiii in $(seq 1 3);
+                    do
+                        echo "<th id=\"$id\" onclick=\"clickHandler($bigField,$smallField,$id)\"></th>"
+                        
+                        id=$(($id+1))
+                        smallField=$(($smallField+1))
+                    done
+                    echo "</tr>"
+                done
+                echo "</table></th>"
+                bigField=$(($bigField+1))
+            done
+            echo "</tr>"
+        done
+
+        echo "</table>"
+    else
+        echo "$line"
+    fi
+
+done < "spiel.html"
