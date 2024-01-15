882fede10a23b664e9b758b37a6630864d0a1ff8
Asmember <76887950+Asmember@users.noreply.github.com>
2024-01-15 23:50:22 +0100

    idk, nix funktioniert

--- game.sh
+++ game.sh
@@ -5,67 +5,81 @@ echo -ne "Content-type: text/html; charset=utf-8\n\n"
 saveDatadir=saveData
 
 read querystring
-eval "${querystring//&/;}"
+
+if [[ "$querystring" != *"="* ]]; then
+    echo "Test"
+    NameInput= `jq ".NameInput" <<< $querystring`
+    numberOfFiles= `jq ".numberOfFiles" <<< $querystring`
+else
+    eval "${querystring//&/;}"
+fi
+
+echo "$querystring"
 
 gameSaveFile="$saveDatadir/$NameInput/games/$numberOfFiles.json"
 
-# reads Game html Template and then replaces the placeholder with the game grid
-while IFS= read -r line
-do
-    if [[ "$line" == *"%GameField%"* ]]; then
-        echo "<table class="game">"
-        id=0
+if [[ -n $smallField ]]; then
+    echo ""
+else
+    # reads Game html Template and then replaces the placeholder with the game grid
+    while IFS= read -r line
+    do
+        if [[ "$line" == *"%GameField%"* ]]; then
+            echo "<table class="game">"
+            id=0
 
-        for i in $(seq 0 2);
-        do
-            echo "<tr>"
-            for ii in $(seq 0 2);
+            for i in $(seq 0 2);
             do
-                echo "<th><table id=\"$i|$ii\" class=\".smallField\">"
-                for iii in $(seq 0 2);
+                echo "<tr>"
+                for ii in $(seq 0 2);
                 do
-                    echo "<tr>"
-                    for iiii in $(seq 0 2);
+                    echo "<th><table id=\"$i|$ii\" class=\".smallField\">"
+                    for iii in $(seq 0 2);
                     do
-                        echo "<th id=\"$id\" onclick=\"clickHandler([$i,$ii],[$iii,$iiii],$id)\">$(jq -r ".GameField[$i][$ii][$iii][$iiii]" $gameSaveFile)</th>"
-                        #                                           Big Field, Small Field
-                        id=$(($id+1))
+                        echo "<tr>"
+                        for iiii in $(seq 0 2);
+                        do
+                            echo "<th id=\"$id\" onclick=\"clickHandler([$i,$ii],[$iii,$iiii],$id)\">$(jq -r ".GameField[$i][$ii][$iii][$iiii]" $gameSaveFile)</th>"
+                            #                                           Big Field, Small Field
+                            id=$(($id+1))
+                        done
+                        echo "</tr>"
                     done
-                    echo "</tr>"
+                    echo "</table></th>"
                 done
-                echo "</table></th>"
+                echo "</tr>"
             done
-            echo "</tr>"
-        done
-        echo "</table>"
+            echo "</table>"
 
-	elif [[ "$line" == *"%Style%"* ]]; then
-		echo "<style>"
-		cat "staticContent/style.css"
-		echo "</style>"
-    elif [[ "$line" == *"%GameData%"* ]]; then
-        echo "<script>"
-        echo "   var gameType = \"$(jq -r ".GameType" $gameSaveFile)\";"
-        echo "   var currentPlayer = \"$(jq -r ".CurrentPlayer" $gameSaveFile)\";"
-        echo "   var lastMove = $(jq -crj .LastMove $gameSaveFile);"
-        echo "   var playerFigures = $(jq -crj .PlayerFigurs $gameSaveFile);"
-        echo "   var spotLight =\"$(jq -r ".BackGroundSpotlightColor" $gameSaveFile)\";"
-        echo "   if(lastMove.length != 0){"
-        echo "      document.getElementById(lastMove[0] + \"|\" + lastMove[1]).style = \"background-color: \" + spotLight + \";\"";
-        echo "   }"
-        echo "</script>"
+        elif [[ "$line" == *"%Style%"* ]]; then
+            echo "<style>"
+            cat "staticContent/style.css"
+            echo "</style>"
+        elif [[ "$line" == *"%GameData%"* ]]; then
+            echo "<script>"
+            echo "   var gameType = \"$(jq -r ".GameType" $gameSaveFile)\";"
+            echo "   var currentPlayer = \"$(jq -r ".CurrentPlayer" $gameSaveFile)\";"
+            echo "   var lastMove = $(jq -crj .LastMove $gameSaveFile);"
+            echo "   var playerFigures = $(jq -crj .PlayerFigurs $gameSaveFile);"
+            echo "   var spotLight =\"$(jq -r ".BackGroundSpotlightColor" $gameSaveFile)\";"
+            echo "   var NameInput =\"$NameInput\";"
+            echo "   var numberOfFiles =\"$numberOfFiles\";"
+            echo "   if(lastMove.length != 0){"
+            echo "      document.getElementById(lastMove[0] + \"|\" + lastMove[1]).style = \"background-color: \" + spotLight + \";\"";
+            echo "   }"
+            echo "</script>"
 
-    elif [[ "$line" == *"%Title%"* ]]; then
-        echo "<title>$(jq -r ".GameType" $gameSaveFile) Game : $(jq -r ".Players[0]" $gameSaveFile) - $(jq -r ".Players[1]" $gameSaveFile)</title>"
-    
-    elif [[ "$line" == *"NameOponent"* ]]; then
-        echo "<h1 id="NameRival">$(jq -r ".Players[0]" $gameSaveFile) VS $(jq -r ".Players[1]" $gameSaveFile)</h1>"
+        elif [[ "$line" == *"%Title%"* ]]; then
+            echo "<title>$(jq -r ".GameType" $gameSaveFile) Game : $(jq -r ".Players[0]" $gameSaveFile) - $(jq -r ".Players[1]" $gameSaveFile)</title>"
+        
+        elif [[ "$line" == *"NameOponent"* ]]; then
+            echo "<h1 id="NameRival">$(jq -r ".Players[0]" $gameSaveFile) VS $(jq -r ".Players[1]" $gameSaveFile)</h1>"
 
-    else
-       echo "$line"
-    fi
-
-done < "spiel.html"
+        else
+        echo "$line"
+        fi
+    done < "spiel.html"
+fi
 
 check_three_in_a_row() {
     local matrix=("$@")
